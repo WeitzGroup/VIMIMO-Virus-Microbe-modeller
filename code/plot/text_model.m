a4b8dda6ddce78323290775f9eeef6a49f55a150
Raunak Dey <39820997+RaunakDey@users.noreply.github.com>
2023-02-27 17:50:49 -0500

    Deleted code directory
    Other scr was added earlier. This is no longer needed

--- code/plot/text_model.m
+++
@@ -1,8 +0,0 @@
-function text_model(model)
-% add model information to an existing figure
-% todo - adds to current axes, make independent
-
-tmpstr = sprintf('%s-%s',model.name,model.odestr());
-text(-.3,0,tmpstr,'Units','normalized','Rotation',90,'HorizontalAlignment','center','Color','red');
-
-end
