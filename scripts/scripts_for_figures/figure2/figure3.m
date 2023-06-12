ef46009fd40b70724501a5220930cf7abbf1d6f6
Raunak Dey <39820997+RaunakDey@users.noreply.github.com>
2023-06-12 18:25:18 -0400

    Script files and generated figures for the paper
    Script files and generated figures for the paper. For testing purposes run these

---
+++ scripts/scripts_for_figures/figure2/figure3.m
@@ -0,0 +1,238 @@
+clear all;
+
+addpath(genpath('/Users/rdey33/Downloads/VIMIMO'));
+
+% import data
+load('data/qpcr','data'); % qpcr data
+load('data/parameters_example','pars'); % parameters without nans
+pars1 = pars;
+load('data/parameters'); % true parameter set with nans
+
+%% simulate trajectory before inference
+
+model = SEIV_diff_NE(5,5,70);
+flags.phi_entire_matrix = 0;
+flags.ssfun_normalized = 0;
+flags.tau_mult = 1;
+flags.mcmc_algorithm = 1; % default is 1 ('dram')
+flags.inference_script = 1;
+flags.confidence_interval = 0;
+flags.tau_new = 0;
+
+pars.NE = 10*(pars.M == 1);
+pars.NE(1,2) = 77;
+pars.NE(2,1) = 74;
+pars.NE(2,2) = 85;
+pars.NE(2,3) = 71;
+pars.NE(3,3) = 55;
+pars.NE(4,4) = 83;
+pars.NE(4,5) = 98;
+pars.NE(5,4) = 87;
+pars.NE(5,5) = 109;
+pars1.NE = pars.NE;
+max_NE = round(max(max(pars.NE)));
+model = SEIV_diff_NE(5,5,max_NE);
+model.host_growth = 0;
+model.viral_decay = 0;
+model.viral_adsorb = 0;
+model.lysis_reset = 0;
+model.debris_inhib = 0;
+
+
+
+
+tvec = 0:0.05:15.75; % for better viz
+
+%% raw data
+load('./../../triplicate_data.mat');
+
+hf = figure
+subplot(2,5,1)
+errorbar(time/60,mean(1e3*host1'),std(1e3*host1'),'o','MarkerSize',8,'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');
+set(gca,'fontname','times')  % Set it to times
+ylim([1e5 1e8]);
+    xlim([0 16]);
+    xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+    yticks([1e5 1e6 1e7 1e8]);
+ylabel({'Host density';'(cells/ml)'});
+title('CBA 4','FontSize',18);
+
+subplot(2,5,2)
+errorbar(time/60,mean(1e3*host2'),std(1e3*host2'),'o','MarkerSize',8,  'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255] );hold on;
+set(gca, 'YScale', 'log');
+set(gca,'fontname','times')  % Set it to times
+ylim([1e5 1e8]);
+    xlim([0 16]);
+   xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+    yticks([1e5 1e6 1e7 1e8]);
+    title('CBA 18','FontSize',18);
+
+
+
+subplot(2,5,3)
+errorbar(time/60,mean(1e3*host3'),std(1e3*host3'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');
+set(gca,'fontname','times')  % Set it to times
+ylim([1e5 1e8]);
+    xlim([0 16]);
+    xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+    yticks([1e5 1e6 1e7 1e8]);
+title('CBA 38','FontSize',18);
+
+
+subplot(2,5,4)
+errorbar(time/60,mean(1e3*host4'),std(1e3*host4'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'FontSize',20)
+set(gca,'fontname','times')  % Set it to times
+ylim([1e5 1e8]);
+    xlim([0 16]);
+  xticks([0 2 4 6 8 10 12 14 16]);
+  axis('square');
+    yticks([1e5 1e6 1e7 1e8]);
+    title('PSA H100','FontSize',18);
+
+
+
+subplot(2,5,5)
+errorbar(time/60,mean(1e3*host5'),std(1e3*host5'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'FontSize',20);
+set(gca,'fontname','times')  % Set it to times
+ylim([1e5 1e8]);
+    xlim([0 16]);
+    xticks([0 2 4 6 8 10 12 14 16]);
+    axis('square');
+    yticks([1e5 1e6 1e7 1e8]);
+    title('PSA 13-15','FontSize',18);
+
+%xlabel("Time (hours)");
+%ylabel("Host density (cell/ml)");
+
+
+
+
+
+
+subplot(2,5,6)
+errorbar(time/60,mean(1e3*virus1'),std(1e3*virus1'),'o','MarkerSize',8,'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'fontname','times')  % Set it to times
+ylim([1e4 1e11]);
+    xlim([0 16]);
+   xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+    yticks([1e4 1e6 1e8 1e10]);
+ylabel({'Phage density';'(virions/ml)'});
+title('\phi18:2','FontSize',18);
+
+
+subplot(2,5,7)
+errorbar(time/60,mean(1e3*virus2'),std(1e3*virus2'),'o','MarkerSize',8,  'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255] );hold on;
+set(gca, 'YScale', 'log');set(gca,'fontname','times')  % Set it to times
+ylim([1e4 1e11]);
+    xlim([0 16]);
+  xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+    yticks([1e4 1e6 1e8 1e10]);
+    title('\phi18:3','FontSize',18);
+
+subplot(2,5,8)
+errorbar(time/60,mean(1e3*virus3'),std(1e3*virus3'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'fontname','times')  % Set it to times
+ylim([1e4 1e11]);
+    xlim([0 16]);
+  xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+   yticks([1e4 1e6 1e8 1e10]);
+   title('\phi38:1','FontSize',18);
+
+subplot(2,5,9)
+errorbar(time/60,mean(1e3*virus4'),std(1e3*virus4'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'fontname','times')  % Set it to times
+ylim([1e4 1e11]);
+    xlim([0 16]);
+   xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+   yticks([1e4 1e6 1e8 1e10]);
+   title('PSA HP1','FontSize',18);
+
+subplot(2,5,10)
+errorbar(time/60,mean(1e3*virus5'),std(1e3*virus5'),'o','MarkerSize',8, 'MarkerEdgeColor','k','MarkerFaceColor',[70/255,130/255,180/255]);hold on;
+set(gca, 'YScale', 'log');set(gca,'fontname','times')  % Set it to times
+ylim([1e4 1e11]);
+    xlim([0 16]);
+ xticks([0 2 4 6 8 10 12 14 16]);
+    set(gca,'FontSize',20);
+    axis('square');
+  yticks([1e4 1e6 1e8 1e10]);
+  title('PSA HS6','FontSize',18);
+
+%% figures
+
+for dummy_beta = 1:50:901
+
+
+for dummy_tau = 0.5:0.5:11
+
+
+
+
+% rewrite pars1
+pars1.beta =[0    0.9400         0         0         0;
+   91.2100   25.700    dummy_beta         0         0;
+         0         0   10.4000         0         0;
+         0         0         0   44.3000  319.00;
+         0         0         0   54.2000  357.800];
+
+pars1.tau = [0    1.0000         0         0         0;
+    1.2500    1.5    dummy_tau         0         0;
+         0         0    1.0000         0         0;
+         0         0         0    0.833    1.5 ;
+         0         0         0    0.6667    1.1667];
+
+pars1.eta(pars1.tau>0) = 1./pars1.tau(pars1.tau>0);
+
+
+pars = pars1;
+
+
+[t1,S1,V1,~] = simulate_ode(model,pars1,tvec,pars1.S0,pars1.V0); % initial parameter set
+
+%color_of_the_lines = [0.75 0.75 0.75];
+%color_of_the_lines = [70/255,210/255,130/255];
+%color_of_the_lines = [111,193,157]./255;
+color_of_the_lines=[171,193,157]./255;
+
+for i = 1:5
+subplot(2,5,i)
+patchline(t1,S1(:,i),'edgecolor',color_of_the_lines,'linewidth',2,'edgealpha',0.1);hold on;
+
+subplot(2,5,i+5)
+patchline(t1,V1(:,i),'edgecolor',color_of_the_lines,'linewidth',2,'edgealpha',0.1);hold on;
+end
+
+
+end
+
+dummy_beta
+end
+
+
+
+han=axes(hf,'visible','off'); 
+han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+set(gca,'FontSize',20);
+set(gca,'fontname','times')  % Set it to times
+xlabel("Time (hours)");
+
