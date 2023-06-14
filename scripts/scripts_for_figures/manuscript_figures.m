30a569c9bb0ad07af5e57de98742a4a832b4f685
Raunak Dey <39820997+RaunakDey@users.noreply.github.com>
2023-06-14 13:04:41 -0400

    testing scripts for manuscript

---
+++ scripts/scripts_for_figures/manuscript_figures.m
@@ -0,0 +1,30 @@
+clc;
+clear all;
+
+%add all the paths
+addpath(genpath('./..'))
+
+%here is figure 1
+cd('./figure1/')
+fig1_parameters;
+figure1;
+cd ..
+
+%Here is Figure 3
+cd('./figure3');
+fig3_condifence;
+
+cd('./..');
+
+%Here is Figure 4
+cd('./figure4')
+fig4_condifence;
+
+cd('./..');
+
+%here is figure 5
+cd('./figure5');
+life_history_compare_new;
+cd('./..');
+
+
