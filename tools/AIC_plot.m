30d911cf95715770eb67955666859886a851d550
RaunakDey <deyraunak111@gmail.com>
2023-08-24 09:40:04 -0400

    aic-redo

--- tools/AIC_plot.m
+++ tools/AIC_plot.m
@@ -3,7 +3,7 @@ clc;
 
 
 
-load('./figure4/SEIVD_datasheet.mat');
+load('SEIVD_datasheet.mat');
 
 % approximately equal.
 sigma_S = (S_max - S_min)./4;
@@ -21,10 +21,10 @@ mu_V = mu_V(1:11:303,:);
 sigma_S = sigma_S(1:11:303,:);
 sigma_V = sigma_V(1:11:303,:);
 
-llf = sum(sum( (-(data_S - mu_S).^2./(2*sigma_S.^2))   + (-(data_V - mu_V).^2./(2*sigma_V.^2)))) + sum(sum(-log(1/sqrt(2*pi)./sigma_S)/log(10)) + sum(-log(1/sqrt(2*pi)./sigma_V)/log(10)))    ;
-AIC(1) = 2*43 - 2*llf
+llf(1) = sum(sum( (-(data_S - mu_S).^2./(2*sigma_S.^2))   + (-(data_V - mu_V).^2./(2*sigma_V.^2)))) + sum(sum(-log(1/sqrt(2*pi)./sigma_S)/log(10)) + sum(-log(1/sqrt(2*pi)./sigma_V)/log(10)))    ;
+AIC(1) = 2*43 - 2*llf(1)
 
-load('./figure3/SEIV_datasheet.mat');
+load('SEIV_datasheet.mat');
 
 
 
@@ -44,8 +44,30 @@ mu_V = mu_V(1:11:303,:);
 sigma_S = sigma_S(1:11:303,:);
 sigma_V = sigma_V(1:11:303,:);
 
-llf = sum(sum( (-(data_S - mu_S).^2./(2*sigma_S.^2))   + (-(data_V - mu_V).^2./(2*sigma_V.^2)))) + sum(sum(-log(1/sqrt(2*pi)./sigma_S)/log(10)) + sum(-log(1/sqrt(2*pi)./sigma_V)/log(10)))    ;
-AIC(2) = 2*42 - 2*llf
+llf(2) = sum(sum( (-(data_S - mu_S).^2./(2*sigma_S.^2))   + (-(data_V - mu_V).^2./(2*sigma_V.^2)))) + sum(sum(-log(1/sqrt(2*pi)./sigma_S)/log(10)) + sum(-log(1/sqrt(2*pi)./sigma_V)/log(10)))    ;
+AIC(2) = 2*42 - 2*llf(2)
+
+
+
+load('./../results/SEIVD-diff-all-seed20011.mat');
+% approximately equal.
+sigma_S = (S_max - S_min)./4;
+sigma_V = (V_max - V_min)./4;
+
+
+mu_S = (S_max + S_min)./2;
+mu_V = (V_max + V_min)./2;
+
+data_S = data.ydata(:,1:5);
+data_V = data.ydata(:,6:10);
+
+mu_S = mu_S(1:11:303,:);
+mu_V = mu_V(1:11:303,:);
+sigma_S = sigma_S(1:11:303,:);
+sigma_V = sigma_V(1:11:303,:);
+
+llf(3) = sum(sum( (-(data_S - mu_S).^2./(2*sigma_S.^2))   + (-(data_V - mu_V).^2./(2*sigma_V.^2)))) + sum(sum(-log(1/sqrt(2*pi)./sigma_S)/log(10)) + sum(-log(1/sqrt(2*pi)./sigma_V)/log(10)))    ;
+AIC(3) = 2*44 - 2*llf(3)
 
 %% plots
 
@@ -73,7 +95,7 @@ b.CData(1,:) = [76,132,147]./255;
 b.CData(2,:) = [217,76,33]./255;
 
 subplot(1,3,3)
-b = bar(x,AIC,'FaceColor','flat');
+b = bar(1:3,AIC,'FaceColor','flat');
 set(gca,'XTickLabel',name);
 ylabel('AIC');
 ylim([1.56e6 1.575e6]);
